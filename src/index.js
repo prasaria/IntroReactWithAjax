f8043191fe98248e0e0fac51ba1f786e3aa1ddd4
prasaria <prasetya.ariawibawa@gmail.com>
2017-12-27 16:07:09 +0700

    Full Intro to React also with AJAX

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
 
 //Create a new component. This component should produce
 //some html
 
 const API_Key = 'AIzaSyBdVj3t0bajPY292Be0BYdVA0ojfWb1SeY';
 
-const App = () => {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    ); 
+
+class App extends Component {
+    constructor(props){
+
+        super(props);
+       
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+      
+        this.videoSearch('marbozir');
+        
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_Key, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos, 
+                selectedVideo: videos[0]
+            });
+         });
+    }
+
+    render() {
+
+        const vidSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
+        return (
+            <div>
+              <SearchBar onSearchTermChange={vidSearch}/>
+              <VideoDetail video={this.state.selectedVideo}/>
+              <VideoList 
+                     onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+                     videos={this.state.videos} />
+            </div>
+          ); 
+    }
+
 }
 
 //Take this component generated HTML and put it on the page(in the DOM)
